020af2c828529ee20bfe41e90b0b5326a8a9e235
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-08 19:57:32 +0800

    add firebase backend

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyCr4lucxNjWC_8N02xCuMstiIkFZKAqObQ",
+    authDomain: "live-chat-7bc8e.firebaseapp.com",
+    projectId: "live-chat-7bc8e",
+    storageBucket: "live-chat-7bc8e.firebasestorage.app",
+    messagingSenderId: "193111320764",
+    appId: "1:193111320764:web:82e32e09dfb01112fa88b9"
+};
+  
+firebase.initializeApp(firebaseConfig);
+let db = firebase.firestore();
+let timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { db, timestamp };
