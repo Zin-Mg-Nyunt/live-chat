4e761f86a6b0384e001266a392b28fdbbae31abe
Zin-Mg-Nyunt <zinmgnyunt99@gmail.com>
2025-04-11 01:49:20 +0800

    watching current user and redirect

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,17 +11,14 @@
 <script>
 
 import logoutUser from "../composable/logoutUser";
-import { useRouter } from 'vue-router';
 import getUser from '../composable/getUser';
 export default {
     setup(){
-        let router = useRouter();
-        
+
         let user = getUser();
 
         let logOut = async()=>{
             await logoutUser();
-            router.push({name:'welcome'})
         }
         return {user,logOut}
     }
--- src/views/ChatroomPage.vue
+++ src/views/ChatroomPage.vue
@@ -3,10 +3,22 @@
 </template>
 
 <script>
+import { watch } from 'vue'
 import NavBar from '../components/NavBar'
+import { useRouter } from 'vue-router';
+import getUser from "../composable/getUser";
 export default {
   components: { NavBar },
-  
+  setup(){
+    let router = useRouter();
+    let user = getUser();
+    // watch method is always watching its first para and if it changes something, sec para run
+    watch(user,()=>{ 
+      if (!user.value) {
+        router.push({name:'welcome'})
+      }
+    })
+  }
 }
 </script>
 
